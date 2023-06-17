682b0dfc7456beedb801d09a257061b6bf33fac4
Shivganesh Digambar Markad <85576625+Shivmarkad@users.noreply.github.com>
2023-06-17 19:32:29 +0530

    Update function.js
    Updated function file

---
+++ function.js
@@ -0,0 +1,13 @@
+// Creating function which takes two arguments and results their sum.
+
+function sum(num1, num2){
+  return num1*num2;
+}
+
+// Creating arrow function 
+
+let myFunction = (a, b) => a * b;
+
+// Without assigning to the variable 
+myfunc = () => "Hello there !!"
+
