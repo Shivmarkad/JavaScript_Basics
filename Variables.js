0c992f7e2db13e69407b7c3ba5db26213b5a4a01
Shivganesh Digambar Markad <85576625+Shivmarkad@users.noreply.github.com>
2023-06-17 19:23:21 +0530

    Update Variables.js
    Edited Variable file

---
+++ Variables.js
@@ -0,0 +1,14 @@
+// Using var
+var num1 = 5;
+var num2 = 6;
+var sum = x + y;
+
+// Using let
+let num1 = 5;
+let num2 = 6;
+let sum = x + y;
+
+// Using const
+const num1 = 5;
+const num2 = 6;
+const sum = x + y;
