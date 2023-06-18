6abcdac29ba523da60eabc118b5e40bc0a883240
Shivganesh <shivmarkad99@gmail.com>
2023-06-18 17:31:03 +0530

    Learn about hoisting in js

---
+++ hoisting.js
@@ -0,0 +1,60 @@
+// // During compile phase, just microseconds before your code is executed, 
+// // it is scanned for function and variable declarations. All these functions and 
+// // variable declarations are added to the memory inside a JavaScript data structure called Lexical Environment. 
+// // So that they can be used even before they are actually declared in the source code.
+
+// // Hoisting Function declaration
+
+// helloWorld();  // prints 'Hello World!' to the console
+// function helloWorld(){
+//   console.log('Hello World!');
+// }
+
+// // The above code will work like : 
+
+// // var helloWorld = function(){
+// //     console.log('Hello World!');  prints 'Hello World!'
+// //   }
+// //   helloWorld();
+  
+
+// // JavaScript only hoist declaration not initialization
+
+
+// // Hoisting variable values 
+
+// console.log(a); // outputs 'undefined'
+// var a = 3;
+
+// // the above code lexical environment look like the var variable will be initialized with indefined 
+
+// // lexicalEnvironment = {
+// //     a: undefined
+// //   }
+  
+// // Hoisting let and const variables:
+
+// // console.log(a);    ReferenceError
+// // let a = 3;
+
+// // All declarations (function, var, let, const and class) are hoisted in JavaScript, 
+// // while the var declarations are initialized with undefined, but let and const declarations remain uninitialized.
+
+// // If the JavaScript engine still can’t find the value of let or const variables at the line
+// //  where they were declared, it will assign them the value of undefined or return an error (in case of const).
+
+// let a;
+// console.log(a); // outputs undefined
+// a = 5;
+
+// // the above code lexicalenvironment will look like :  
+// // lexicalEnvironment = {
+// //     a: <uninitialized>
+// //   }
+
+// After first execution  
+//      lexicalEnvironment = {
+// //     a: undefined
+// //   }
+
+
