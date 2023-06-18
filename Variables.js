65619c79924a306f4db304a2b050b8bf1e7c7ab5
Shivganesh <shivmarkad99@gmail.com>
2023-06-18 13:28:38 +0530

    Updated variable file

--- Variables.js
+++ Variables.js
@@ -1,16 +1,16 @@
 // Using var
-var num1 = 5;
-var num2 = 6;
-var sum1 = x + y;
+var a = 5;
+var b = 6;
+var sum1 = a + b;
 console.log(sum1)
 
 // Using let
-let num1 = 5;
-let num2 = 6;   
-let sum2 = x + y;
+let c = 5;
+let d = 6;   
+let sum2 = d + c;
 console.log(sum2)
 // Using const
-const num1 = 5;
-const num2 = 6;
-const sum3 = x + y;
+const e = 5;
+const f = 6;
+const sum3 = e + f;
 console.log(sum3)
