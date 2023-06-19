6ab6e835946549a71e44f86af10ac55e758addb2
Shivganesh <shivmarkad99@gmail.com>
2023-06-19 17:08:01 +0530

    Updated var varible infor the file

--- Variables.js
+++ Variables.js
@@ -1,8 +1,46 @@
 // Using var
+// var and let create variables that can be reassigned another value.
+
 var a = 5;
 var b = 6;
 var sum1 = a + b;
 console.log(sum1)
+
+// Global scope variable
+
+var number = 50    // As it is declared outside of the function so has global scope 
+
+function print() {
+  var square = number * number
+  console.log(square)
+}
+
+console.log(number) // 50
+
+print() // 2500
+
+//Local scope variable 
+function print() {
+    var num = 50              // As it is declared inside the function so it has local scope 
+    var square = num * num    // It can be accessed inside the local scope only 
+    console.log(square)
+  }
+  
+  print() // 2500
+  
+  console.log(num)            // Out the scope/function will get not defined error
+  // ReferenceError: number is not defined
+
+// var keyword allows redeclaration
+var num1 = 50
+console.log(num1) // 50
+
+var num1 = 100
+console.log(num1) // 100
+
+// var also allows reinitialization
+num1 = 20
+console.log(num1) // 20
 
 // Using let
 
@@ -12,6 +50,7 @@ let d = 6;
 let sum2 = d + c;
 console.log(sum2)
 
+// const creates "constant" variables that cannot be reassigned another value.
 
 // Using const
 const e = 5;
