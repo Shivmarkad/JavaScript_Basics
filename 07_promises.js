50898904e51240f06e7ca093b0eeb3f92ccb2074
Shivganesh <shivmarkad99@gmail.com>
2023-06-20 20:28:27 +0530

    Updated promises handled error for promises

--- 07_promises.js
+++ 07_promises.js
@@ -1,32 +1,43 @@
 // // In JavaScript, promises are special objects that help you perform asynchronous operations.
 
 function f1() {
-    console.log('f1');
+    console.log("This is function 1");
 }
 
 function f2() { 
-    console.log('f2');
+    console.log('This is function 2');
 }
 
 function f3() { 
-    console.log('f3');
+    console.log('This is function 3');
 }
 
 function main() {
     console.log('main');
 
-    setTimeout(f1, 50);
-    setTimeout(f3, 30);
+    setTimeout(f1, 50);             //Setting timing for function execution
+    setTimeout(f3, 30);             //Setting timing for function execution 
 
     new Promise((resolve, reject) =>
-    resolve('I am a Promise, right after f1 and f3!')
-    ).then(resolve => console.log(resolve));
+        resolve('I am a first Promise for resolve'))
+        .then(resolve1 => console.log(resolve1))
+        .catch(error1 => console.log(error1));
     
-    new Promise((resolve, reject) =>
-    resolve('I am a Promise after Promise!')
-    ).then(resolve => console.log(resolve));
+    new Promise((resolve, reject) =>{
+
+        setTimeout(() => {
+            console.log("This is a promise for rejection")        //This will print aftet this promise 
+        }, 0);
+
+        console.log("This is a promise for rejection /console")            //This will print after main
+        
+        reject('I am rejection')
+    }
+    )
+    .then(resolve => console.log(resolve))
+    .catch(error => console.log(error));
 
     f2();
 }
 
-main();
+main();                                             //Calling the main function 
