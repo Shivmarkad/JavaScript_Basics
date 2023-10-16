82a48787fafc519cb52d930e7a3e85450f307d1c
Shivganesh <shivmarkad99@gmail.com>
2023-10-16 08:15:18 +0530

    [shivganesh] practice: map data structure

--- map_data.js
+++ map_data.js
@@ -4,10 +4,10 @@
 
 const map1 = new Map();
 
-map1.set('a', 1);
-map1.set('b', 2);
-map1.set('c', 3);
-map1['hi'] = 23
+map1.set('myname', 'shivganesh');
+map1.set('age', 24);
+map1.set('role', 'developer');
+map1['newrole'] = 'designer'
 console.log(map1)
 
 //Maps can be iterated using a for...of loop:
@@ -17,17 +17,17 @@ for(const [key,val] of map1){
 }
 
 //get method to get the value with key
-console.log(map1.get('a'));
+console.log(map1.get('age'));
 
 //set method to set the value with key
-map1.set('a', 97);
+map1.set('age', 23);
 
-console.log(map1.get('a'));
+console.log(map1.get('age'));
 
 //size to get the size of map data structure
 console.log(map1.size);
 
 //delete method to delete the value with key 
-map1.delete('b');
+map1.delete('role');
 
 console.log(map1.size);
