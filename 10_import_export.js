b8c7fa748fc45f63249c8d7c6057b06092b61c20
Shivganesh <shivmarkad99@gmail.com>
2023-10-16 06:52:56 +0530

    [Shivganesh] practice: export import statements

--- 10_import_export.js
+++ 10_import_export.js
@@ -1,7 +1,10 @@
 
-// Export: You can export a variable using the export keyword    in front of that variable declaration. You can also export a function and a class by doing the same.
+// Export: You can export a variable using the export keyword    
+// in front of that variable declaration. You can also export a function 
+// and a class by doing the same.
 
-// Import: You can import a variable using import keyword. You can specify one of all the members that you want to import from a JavaScript file.
+// Import: You can import a variable using import keyword. 
+//You can specify one of all the members that you want to import from a JavaScript file.
 
 
 import hello1, {hello}   from "08_async_await";
@@ -9,3 +12,22 @@ import hello1, {hello}   from "08_async_await";
 hello1();
 hello();
 
+//exporting a function
+
+export const myfunction = function (arr) {
+    let evenNum = arr.filter(item => item%2 ==0)
+    return evenNum;
+}
+
+//creating function for default export
+
+const getSum = function(arr){
+    let sum = arr.reduce((result, item)=>{
+        return result + item
+    })
+    return sum
+}
+
+//exporting above function by default
+
+export default getSum;
