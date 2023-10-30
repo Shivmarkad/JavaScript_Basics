2aff40f17b7ec23ccef2b93dc0cb2ca699605a17
Shivganesh <shivmarkad99@gmail.com>
2023-10-30 16:14:38 +0530

    [Shivganesh] Practice: callback function in js

---
+++ JS_Basics/04_callback.js
@@ -0,0 +1,15 @@
+// A callback function is a function passed into another function as an argument.
+
+// function
+function greet(name, callback) {
+    console.log('Hi' + ' ' + name);
+    callback();
+}
+
+// callback function
+function callMe() {
+    console.log('I am callback function');
+}
+
+// passing function as an argument
+greet('Shiv', callMe);
