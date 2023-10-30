7e165be15edef6979c0457a5b7da12b08d1a98c8
Shivganesh <shivmarkad99@gmail.com>
2023-10-30 14:31:47 +0530

    [Shivganesh] Practice: variables in javascript

--- JS_Basics/01_variables.js
+++ JS_Basics/01_variables.js
@@ -29,3 +29,53 @@ p()
 
 var myVariable = 90
 console.log(myVariable)
+
+// var variables declared inside statements are accessible outside for global
+
+if(true){
+    var age = 24;
+}
+console.log(age)
+
+// *** 2 - using const keyword ***
+
+// const and let variables will hoisted as uninitialized in lexical scope
+
+const name = "shiv";
+
+// const variables cannot be redeclared
+// const name = "Shivganesh"  
+
+// const variables cannot be modified
+// name = "Shivganesh"
+
+// const is block-scoped, whereas var is function-scoped.
+
+function returnVal() {
+    const val = 1
+    return val;
+}
+
+console.log(returnVal())
+
+// *** 3 - using let keyword *** 
+
+// let variables can be reinitialized but cannot be redeclared
+
+let property = "fly";
+
+// Reinitialized
+property = "swim"
+
+// cannot be redeclared
+// let property = "run"
+
+// let variable has block scope means that it cannot be accessed outside the block
+
+function getDetails() {
+    let myDetails = "Shivganesh Markad";
+    console.log(myDetails)
+}
+
+getDetails()
+// console.log(myDetails) // error - myDetails is not defined
