54ac8558df677782f6d13345459e65471fcc97e2
Shivganesh <shivmarkad99@gmail.com>
2023-11-04 14:40:05 +0530

    [Shivganesh] Refactor: synchronous and asynchronous execution in JS

--- 03_Synchronous_vs_Asynchronous.js
+++ 03_Synchronous_vs_Asynchronous.js
@@ -1,21 +1,12 @@
 
 // JavaScript is a single-threaded, non-blocking, asynchronous, concurrent programming language with lots of flexibility.
 
-// By default, every line in a function executes sequentially, one line at a time. The same is applicable even when you invoke multiple functions in your code. Again, line by line.
+// By default, every line in a function executes sequentially, one line at a time.
+// The same is applicable even when you invoke multiple functions in your code. Again, line by line.
 
-// The JavaScript engine maintains a stack data structure called function execution stack. The purpose of the stack is to track the current function in execution.
+// The JavaScript engine maintains a stack data structure called function execution stack. 
+// The purpose of the stack is to track the current function in execution.
 
-/*It does the following:
-
-When the JavaScript engine invokes a function, it adds it to the stack, and the execution starts.
-If the currently executed function calls another function, the engine adds the second function to the stack and starts executing it.
-Once it finishes executing the second function, the engine takes it out from the stack.
-The control goes back to resume the execution of the first function from the point it left it last time.
-Once the execution of the first function is over, the engine takes it out of the stack.
-Continue the same way until there is nothing to put into the stack.
-
-The function execution stack is also known as the Call Stack.
-*/
 //Synchronous execution 
 function f1(a, b) {
   sum = a + b;
@@ -53,9 +44,9 @@ function func1(){
 
 function func2(){
   console.log("it is my 2nd function)");
-  func1();                                //here we called the function inside of an another function
+  func1();               //here we called the function inside of an another function
 }
-func2();                                  //Here the function invoked func2 will also invoke func 1
+func2();                //Here the function invoked func2 will also invoke func 1
 
 //88888888888888888888888888888888888888888888888888888888888888888888888888888888
 
