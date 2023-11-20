74462253cffbb62e88447f19fde9a27c2415aac7
shivganesh <shivmarkad99@gmail.com>
2023-11-20 17:04:57 +0530

    [Shivganesh] Practice: array and object destructuring in javascript

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+const arr = ["shiv", 23, "suresh"]
+
+const obj = {
+    namee: "sanjay",
+    age: 23
+}
+
+const { namee } = obj
+const [ n, m ] = arr
+
+console.log(namee)
+console.log(n)
+console.log(m)
