075e9b4cd46515376367bc02c5f91756e477f63f
shivganesh <shivmarkad99@gmail.com>
2023-11-23 10:48:41 +0530

    [ Shivganesh ] Practice: variable declaration in javascript

---
+++ practice/01_variables.js
@@ -0,0 +1,48 @@
+
+// There are three type of variable declaration 
+
+// var, let and const
+
+// var variable has global and functional scope 
+
+// let and const variable has global and block scope
+
+// var declared variable can be redeclared and reinitialized 
+
+// let declared variable can not be redeclared but can be reinitialized i.g. value can be changed
+
+// const declared variable can not be redeclared or reinitialized, must be initialized whenever declared
+
+// var
+
+var age = 12;
+
+var age = 23;       // redeclared and reinitialized
+
+function getAge(){
+    var newAge = 45;     
+    console.log(newAge)  // can be accessed only inside function
+    return age;         // accessing global variable declared with var
+}
+
+// console.log(newAge)     // ReferenceError: newAge is not defined
+
+console.log(getAge())
+
+// let
+
+let yourName = "Shiv";
+
+yourName = "Ganesh";        // can be reinitialized
+
+console.log(yourName)
+
+// can not be redeclared 
+// let yourName = 34;  // SyntaxError: Identifier 'yourName' has already been declared
+
+// const
+
+const myRole = "Dev";
+
+// const myRole = 23;   // SyntaxError: Identifier 'myRole' has already been declared
+console.log(myRole)
